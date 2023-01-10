3f77ecdd93fb7134874fef8add6752c59d248985
Andrew Murrow <murrr.andrew@gmail.com>
2023-01-10 22:34:36 +0200

    Django: ListView

--- backend/movies/views.py
+++ backend/movies/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import Movie
+
+
+class MoviesView(ListView):
+    model = Movie
+    template = 'movies/movies_view_list.html'
+
