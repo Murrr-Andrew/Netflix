cfcd51a407a98b489fe7a9a731dd1a8786a6a3bc
Andrew Murrow <murrr.andrew@gmail.com>
2023-01-12 19:23:08 +0200

    Django: Update urls.py

--- backend/movies/urls.py
+++ backend/movies/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import MovieListView, MovieDetailView
 
 urlpatterns = [
-    path('', MovieListView.as_view(), name='home'),
+    path('', MovieListView.as_view(), name='movie_list'),
     path('<str:slug>/', MovieDetailView.as_view(), name='movie_detail')
 ]
