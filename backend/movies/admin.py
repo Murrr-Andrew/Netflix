a63979e0ea82b6ba37f2e1ecface4392c3b3f452
Andrew Murrow <murrr.andrew@gmail.com>
2023-01-19 20:21:43 +0200

    Django: Admin

--- backend/movies/admin.py
+++ backend/movies/admin.py
@@ -2,11 +2,59 @@ from django.contrib import admin
 from .models import Category, Genre, Movie, MovieShots, Actor, Rating, RatingStar, Reviews
 
 
-admin.site.register(Category)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'slug')
+    list_display_links = ('name', )
+
+
+class ReviewInline(admin.TabularInline):
+    model = Reviews
+    extra = 1
+    readonly_fields = ('name', 'email')
+
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'slug', 'draft')
+    list_filter = ('category', 'year')
+    search_fields = ('title', 'category__name')
+    inlines = [ReviewInline]
+    save_on_top = True
+    save_as = True
+    list_editable = ('draft', )
+
+    fieldsets = (
+        (None, {
+            'fields': (('title', 'tagline'), )
+        }),
+        (None, {
+            'fields': (('description', 'poster'), )
+        }),
+        (None, {
+            'fields': (('year', 'world_premiere', 'country'), )
+        }),
+        ('Actors', {
+            'classes': ('collapse', ),
+            'fields': (('actors', 'producers', 'genres', 'category'), )
+        }),
+        (None, {
+            'fields': (('budget', 'fees_in_usa', 'fees_in_world'), )
+        }),
+        ('Options', {
+            'fields': (('slug', 'draft'), )
+        })
+    )
+
+
+@admin.register(Reviews)
+class ReviewsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'parent', 'movie', 'id')
+    readonly_fields = ('name', 'email')
+
+
 admin.site.register(Genre)
-admin.site.register(Movie)
 admin.site.register(MovieShots)
 admin.site.register(Actor)
 admin.site.register(Rating)
 admin.site.register(RatingStar)
-admin.site.register(Reviews)
