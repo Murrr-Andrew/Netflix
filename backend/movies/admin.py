a3154df6721042350e5de5f306beb38eaee47654
Andrew Murrow <murrr.andrew@gmail.com>
2023-01-19 20:39:18 +0200

    Django: Admin (part 3)

--- backend/movies/admin.py
+++ backend/movies/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
 from .models import Category, Genre, Movie, MovieShots, Actor, Rating, RatingStar, Reviews
 
 
@@ -60,7 +62,13 @@ class GenreAdmin(admin.ModelAdmin):
 
 @admin.register(Actor)
 class ActorAdmin(admin.ModelAdmin):
-    list_display = ('name', 'age')
+    list_display = ('name', 'age', 'get_image')
+    readonly_fields = ('get_image', )
+
+    def get_image(self, obj):
+        return mark_safe(f'<img src="{obj.image.url}" width="50" height="60" />')
+
+    get_image.short_description = 'Image'
 
 
 @admin.register(Rating)
